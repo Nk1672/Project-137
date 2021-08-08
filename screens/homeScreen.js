57486e49a548baf952404b6659baebf9cb74ee48
Nk1672 <69999505+Nk1672@users.noreply.github.com>
2021-08-07 23:15:01 -0700

    Add files via upload

---
+++ screens/homeScreen.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+import {View, Text, StyleSheet, Button, Alert, FlatList, SafeAreaView} from 'react-native'
+import {ListItem} from 'react-native-elements'
+
+export default class HomeScreen extends React.Component{
+
+    render(){
+        return(
+            <Text>HomeScreen</Text>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#edc988"
+    },
+    upperContainer: {
+      flex: 0.1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    headerText: {
+      fontSize: 30,
+      fontWeight: "bold",
+      color: "#132743"
+    },
+    lowerContainer: {
+      flex: 0.9
+    },
+    emptyContainer: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center"
+    },
+    emptyContainerText: {
+      fontSize: 20
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: "bold",
+      color: "#d7385e"
+    },
+    listContainer: {
+      backgroundColor: "#eeecda"
+    }
+  });
